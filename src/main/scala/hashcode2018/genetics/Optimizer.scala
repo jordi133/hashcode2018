696147e20cfb749b82d33fc9ae2a2a8c22d121ba
Jordi de Vos <devos.jordi@gmail.com>
2018-03-05 22:07:08 +0100

    work in progress

---
+++ src/main/scala/hashcode2018/genetics/Optimizer.scala
@@ -0,0 +1,11 @@
+package hashcode2018.genetics
+
+import hashcode2018.InputData
+
+object Optimizer {
+
+  def optimize(input: InputData) = {
+    ???
+  }
+
+}
---
+++ src/main/scala/hashcode2018/genetics/Planning.scala
@@ -0,0 +1,7 @@
+package hashcode2018.genetics
+
+import hashcode2018._
+
+case class Planning(vehicles: Seq[Seq[Ride]]) {
+
+}
